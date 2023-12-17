d78d31b674ca32a73cd69e893f8a6e2090871aae
yahyojondev <nabijonovdeveloper@gmail.com>
2023-12-18 04:55:42 +0500

    dhdibs

---
+++ js/while.js
@@ -0,0 +1,139 @@
+// while 1
+
+// let A = +prompt("A ni kiriting");
+// let B = +prompt("B ni kiriting");
+
+
+// while(A >= B ){
+//     A -= B;
+// }
+// console.log(A);
+
+// while 2
+
+// let A = +prompt("A ni kiriting")
+// let B = +prompt("B ni kiriting")
+// let c = 0;
+
+// while(A >= B){
+//     A -= B;
+//     c++;
+// }
+
+// console.log(c);
+
+// while 3
+
+// let n = +prompt("n ni kiriting");
+// let p =1;
+
+// while(n > p){
+//   p= p * 3 ;
+//     if(p == n){
+//         console.log(n,"3 ning darajsi");
+//     }else{
+//         console.log(n, "3 ning darajasi emas")
+//     }
+
+// }
+
+// while 4
+
+// let n = +prompt("n soni kiriting");
+// let m = +prompt("m soni kiriting");
+// let c =0;
+
+// while(n >= m){
+//     n -= m;
+//     c++;
+// }
+// console.log(c);
+// console.log(n)
+
+// while 5
+// function reverseDigits(n) {
+//   let reversedNum = 0;
+//   while (n > 0) {
+//     let remainder = n % 10;
+//     reversedNum = reversedNum * 10 + remainder;
+//     n = Math.floor(n / 10);
+//   }
+//   return reversedNum;
+// }
+
+// // Foydalanuvchidan sonni kiritishni so'raymiz
+// let n = parseInt(prompt("Istalgan butun sonni kiriting:"));
+
+// // Sonni teskari tartibda chiqaramiz
+// let reversed = reverseDigits(n);
+// console.log(reversed);
+
+// while 6
+
+// function reverseDigits(n) {
+//   c = 0;
+//   while (n > 0) {
+//     let remainder = n % 10;
+//    c += remainder
+//     n = Math.floor(n / 10);
+//   }
+//   return c, remainder;
+// }
+
+// // Foydalanuvchidan sonni kiritishni so'raymiz
+// let n = parseInt(prompt("Istalgan butun sonni kiriting:"));
+
+// console.log(c);
+
+// while 7
+// let n = 1234;
+// while(n != 0){
+//    if( n % 10 === 2){
+    
+//        console.log(n, "da 2 raqami bor");
+//    }else{
+//          console.log(n, "da 2 raqami yoq")
+//    }
+// }
+
+// while 8
+
+// let n =1234;
+
+// while(n > 0){
+//     if(n % 2 == 1){
+//         console.log(n,"sonida toq raqam bor");
+//     }else{
+//         console.log(n,"sonida toq raqam yoq")
+//     }
+// }
+
+// while 9
+
+// let n = 1234321;
+// let pull = 0;
+// let c;
+
+// while(n != 0){
+//     let pull = n % 10;
+//     c += math.string(pull);
+//     if(n == c){
+//         console.log(n,"soni polindromik");
+//     }else{
+//         console.log(n,"soni polindromik emas")
+//     }
+// }
+
+// while 10
+
+// let i = 2;
+// while(n > i * i){
+//      if (n <= 1) {
+//        console.log(false)
+//     }
+//     if((n % i == 0  ))
+//     console.log(true);
+//     i++;
+//     }
+
+
